3f8fade54317fd42b391b685c505a46c70c351a2
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-11-22 12:41:51 -0600

    ListingDetailView added

--- AirBnBApp/Core/Listings/ListingDetailView.swift
+++ AirBnBApp/Core/Listings/ListingDetailView.swift
@@ -6,13 +6,33 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ListingDetailView: View {
-    
+    @Environment(\.presentationMode) var presentatioMode
+    ///Action to dismiss
+   
     var body: some View {
         ScrollView {
-            ListingImageCarruselView()
-            .frame(height: 320)
+            ZStack(alignment: .topLeading) {
+                ListingImageCarruselView()
+                    .frame(height: 320)
+                
+                Button {
+                    presentatioMode.wrappedValue.dismiss()
+                } label: {
+                    Image(systemName: "chevron.left")
+                        .foregroundStyle(.black)
+                        .background {
+                            Circle()
+                                .fill(.white)
+                                .frame(width: 32, height: 32)
+                        }
+                        .padding(32)
+                        
+                }
+
+            }
             
             VStack(alignment: .leading, spacing: 8) {
                 Text("Marco's house")
@@ -69,6 +89,129 @@ struct ListingDetailView: View {
             .padding()
             
             Divider()
+            
+            // listing features
+            VStack(alignment: .leading, spacing: 16) {
+                ForEach(0 ..< 2) { feature in
+                    HStack(spacing: 12) {
+                        Image(systemName: "medal")
+                        
+                        VStack(alignment: .leading) {
+                            Text("Superhost")
+                                .font(.footnote)
+                                .fontWeight(.semibold)
+                            
+                            Text("Superhost are experience, highly rated hosts who are commited to providing great stars for guests.")
+                                .font(.caption)
+                                .foregroundStyle(.gray)
+                        }
+                        
+                        Spacer()
+                    }
+                }
+            }
+            .padding()
+            
+            Divider()
+            
+            // bedrooms view
+            VStack(alignment: .leading, spacing: 16) {
+                Text("Where you'll sleep")
+                    .font(.headline)
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 16) {
+                        ForEach(1 ..< 6) { bedroom in
+                            VStack {
+                                Image(systemName: "bed.double")
+                                
+                                Text("Bedroom \(bedroom)")
+                            }
+                            .frame(width: 132, height: 100)
+                            .overlay {
+                                RoundedRectangle(cornerRadius: 12)
+                                    .stroke(lineWidth: 1)
+                                    .foregroundStyle(.gray)
+                            }
+                        }
+                    }
+                }
+                .scrollTargetBehavior(.paging)
+                
+            }
+            .padding()
+            
+            Divider()
+            
+            // listing ameneties
+            VStack(alignment: .leading, spacing: 16) {
+                Text("What this place offers")
+                    .font(.headline)
+                
+                ForEach(0 ..< 6) { feature in
+                    HStack {
+                        Image(systemName: "wifi")
+                            .frame(width: 32)
+                        
+                        Text("Wifi")
+                        
+                        Spacer()
+                    }
+                }
+            }
+            .padding()
+            
+            Divider()
+
+            VStack(alignment: .leading, spacing: 16) {
+                Text("Where you'll be")
+                    .font(.headline)
+                
+                Map()
+                    .frame(height: 200)
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
+            }
+            .padding()
+        }
+        .ignoresSafeArea()
+        .padding(.bottom, 64)
+        .overlay(alignment: .bottom) {
+            VStack {
+                Divider()
+                    .padding(.bottom)
+                
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("899")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                        
+                        Text("Total before taxes")
+                            .font(.footnote)
+                        
+                        Text("Nov 20 - 25")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                            .underline()
+                    }
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("Reserve")
+                            .foregroundStyle(.white)
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .frame(width: 140, height: 40)
+                            .background(.pink)
+                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                    })
+                }
+                .padding(.horizontal, 32)
+            }
+            .background(.white)
         }
     }
 }
