4a1d2a008668ed1333e3f8fd85ea98bcc82b8c8c
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-11-28 09:35:45 -0600

    Destination Search View 5 added with animations

--- AirBnBApp/Core/Explore/ExploreView.swift
+++ AirBnBApp/Core/Explore/ExploreView.swift
@@ -8,11 +8,21 @@
 import SwiftUI
 
 struct ExploreView: View {
+    @State private var showDestinationSearchView = false
+    
     var body: some View {
         NavigationStack {
-            VStack {
-                SearchAndFilterBar()
+            if showDestinationSearchView {
+                DestinationSearchView(show: $showDestinationSearchView)
+            } else {
                 ScrollView {
+                    SearchAndFilterBar()
+                        .onTapGesture {
+                            withAnimation(.snappy) {
+                                showDestinationSearchView.toggle()
+                            }
+                        }
+                    
                     LazyVStack(spacing: 32) {
                         ForEach(0 ... 10, id: \.self) { listing in
                             NavigationLink(value: listing) {
@@ -22,14 +32,13 @@ struct ExploreView: View {
                             }
                         }
                     }
-                }
-                .navigationDestination(for: Int.self) { listing in
-                    ListingDetailView()
-                        .navigationBarBackButtonHidden()
-                }
-            }
-            
-        }
+                    .navigationDestination(for: Int.self) { listing in
+                        ListingDetailView()
+                            .navigationBarBackButtonHidden()
+                    }
+                }// scrollView
+            }// else
+        }//NavStack
     }
 }
 
